1a439fc45efe52b864c449e2e0a3a5330aab26ff
Akobir <akobirqochqorov07@gmail.com>
2026-04-20 13:47:23 +0500

    fix: routing for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Admin } from './pages/Admin';
 
 function App() {
   return (
-    <Router basename="/tozago">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<Admin />} />
